5378d3c3671b09e8e44e56817c622d0be0aa2d72
A.C.E <amienghemenchristian948@gmail.com>
2023-09-18 16:34:05 +0100

    finished the staff overview component

--- src/components/Dashboard/Staff/StaffOverview.js
+++ src/components/Dashboard/Staff/StaffOverview.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Sidebar from "../Sidebar";
 import "./StaffOverview.css";
+import icon from "../../../assets/list_check_icon_151227.png";
+import students from '../../../assets/profile-3user.svg'
 
 const StaffOverview = () => {
   const overviewData = [
     {
       title: "No of students",
       value: 140,
-      icon: "",
+      icon: students,
     },
     {
       title: "Current Cohorts",
@@ -51,42 +53,60 @@ const StaffOverview = () => {
 
   return (
     <div className="overviewArea">
+      <div className="topLine"></div>
       <div className="overviewHeader">
-        <h1>Overview</h1>
+        <h2>Overview</h2>
+        <div className="overviewTextDate">
+          <div className="datePicker">
+            <span>From: </span>
+            <input type="date" />
+            <span>To: </span>
+            <input type="date" />
+          </div>
+          <button className="btn createBtn">
+            <span>Create</span>
+          </button>
+        </div>
       </div>
+      <div className="bottomLine"></div>
       <div className="overviewBody">
-        <div className="overviewCard">
-          {overviewData.map((overview) => (
-            <div className="overviewCardContent">
-              <h5>{overview.title}</h5>
-              <div className="overview-value-icon">
-                <h2>{overview.value}</h2>
-                <span>{overview.icon}</span>
+        <div className="overviewStatistics">
+          {overviewData.map((stats) => (
+            <div className="overviewCard">
+              <h5>{stats.title}</h5>
+              <div className="valueIcon">
+                <h2>{stats.value}</h2>
+                <img src={stats.icon} alt="" />
               </div>
             </div>
           ))}
         </div>
-
-        <div className="taskMangement">
-          <div className="taskManagementHeader">
-            <h3>Task management</h3>
-            <p>See all</p>
-          </div>
-          <div className="taskManagementBody">
-            <div className="taskManagementTasks">
-              <div className="taskManagementContent">
-                {taskmanagement.map((task) => (
-                  <div className="taskManagementText">
-                    <h4>{task.title}</h4>
-                    {/* <div className="taskManagementContentIcon"></div> */}
-                    <div className="taskManagementSubTexts">
-                      <span>{task.course}</span> 
-                      <span> {task.cohort}</span>
-                    </div>
-                  </div>
-                ))}
+        <div className="taskAttendance">
+          <div className="taskManagement">
+            <div className="taskManagementHeader">
+              <div className="taskManagementTitle">
+                <img src={icon} alt="" className="taskManagementIcon" />
+                <strong>Task Management</strong>
               </div>
-              <div className="taskManagementIcon"></div>
+              <p>See all</p>
+            </div>
+            {taskmanagement.map((tasks) => (
+              <div className="taskDetails">
+                <h5>{tasks.title}</h5>
+                <div className="taskSubdetails">
+                  <span>{tasks.course}</span>
+                  <span>{tasks.cohort}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="attendanceTracking">
+            <div className="attendanceHeader">
+              <h5>Attendance tracking</h5>
+              <select name="cohorts" id="cohorts">
+                <option value="cohort1">Cohort 1</option>
+                <option value="cohort1">Cohort 2</option>
+              </select>
             </div>
           </div>
         </div>
